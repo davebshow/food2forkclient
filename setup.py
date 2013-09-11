1831bc70b2e1d8dd91d80ef9cab6f69205f6042c
davidmichaelbrown <davidmichaelbrown1@gmail.com>
2013-09-10 21:58:45 -0400

    added setup, moved client.py to seperate directory

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name                = 'food2forkclient',
+    version             = open( 'VERSION' ).read(),
+    description         = 'Python package for Food2Fork API http://food2fork.com/api',
+    long_description    = open( 'README' ).read(),
+    author              = 'David Michael Brown',
+    author_email        = 'davidmichaelbrown1@gmail.com',
+    url                 = 'https://github.com/davidmichaelbrown/food2forkclient',
+    packages            = [ 'food2forkclient', 'food2fork_tests' ],
+    install_requires    = open( 'requirements.txt' ).read().split(),
+    keywords            = 'food2fork recipes',
+    license             = 'MIT',
+    classifiers         = [
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'License :: MIT'
+    ]
+)
+
