1ad3350fe41c05d928d556d1f9ec20421c3a826e
davidmichaelbrown <davidmichaelbrown1@gmail.com>
2013-09-11 17:45:24 -0400

    cleaning up formatting errors

--- food2forkclient/tests.py
+++ food2forkclient/tests.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import os
 import unittest
 
 import config
@@ -7,13 +6,11 @@ from client import Food2ForkClient
 
 API_KEY = getattr(config, 'API_KEY', None)
 
-class TestFood2ForkClient(unittest.TestCase):
 
+class TestFood2ForkClient(unittest.TestCase):
 
     def setUp(self):
         self.f2fclient = Food2ForkClient(api_key=API_KEY)
-
-
 
     def test_api_url(self):
         self.assertEqual(
@@ -21,23 +18,25 @@ class TestFood2ForkClient(unittest.TestCase):
         )
 
     def test_search_url(self):
-        self.assertEqual(self.f2fclient.URL_SEARCH, 'http://food2fork.com/api/search/?')
+        self.assertEqual(self.f2fclient.URL_SEARCH,
+                         'http://food2fork.com/api/search/?')
 
     def test_get_url(self):
-        self.assertEqual(self.f2fclient.URL_GET, 'http://food2fork.com/api/get/?')
+        self.assertEqual(self.f2fclient.URL_GET,
+                         'http://food2fork.com/api/get/?')
 
     def test_search(self):
         header, response = self.f2fclient.search()
         self.assertTrue(
-            len(header) > 0 and \
-            response['count'] == 30 and \
+            len(header) > 0 and
+            response['count'] == 30 and
             len(response['recipes']) == 30
         )
 
     def test_query_search(self):
         header, response = self.f2fclient.search(q='chicken')
         self.assertTrue(
-            len(header) > 0 and \
+            len(header) > 0 and
             len(response['recipes']) > 0
         )
 
@@ -47,6 +46,6 @@ class TestFood2ForkClient(unittest.TestCase):
         self.assertTrue(len(response['recipe']) > 1)
 
 
-
 if __name__ == '__main__':
     unittest.main()
+    
